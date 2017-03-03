70c841d4d265139bc416cd11efe9bf52f0d60822
nlaliberte <nathan.laliberte@gmail.com>
2017-03-03 11:00:20 -0500

    Renamed files

--- schedule_db/dbo/Tables/conference.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[conference] (
-    [league_id]       INT           NOT NULL,
-    [conference_id]   INT           NOT NULL,
-    [conference_name] VARCHAR (255) NOT NULL,
-    CONSTRAINT [pk_conference] PRIMARY KEY CLUSTERED ([league_id] ASC, [conference_id] ASC),
-    CONSTRAINT [unq_conference_name] UNIQUE NONCLUSTERED ([league_id] ASC, [conference_name] ASC)
-);
-
